b33d44b81c92ff96850701575cfecfed114536af
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-09-08 16:11:32 +0200

    added Terminal Commands

--- demo/app.js
+++ demo/app.js
@@ -1,3 +1,13 @@
+/*--------------------------------------------------------*
+*			Students RestApi - NodeJS, Express  		  *
+* --------------------------------------------------------*
+*                   Terminal commands:                    *
+*                       								  *	
+*       		Install: 	cd demo                       *
+*               			npm install                   *
+*       		Run: 		node app.js              	  *
+* --------------------------------------------------------*/
+
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
